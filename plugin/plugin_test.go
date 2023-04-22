b104a02a1f97fc0209566516e28dd8fe47aa2d07
shivansh verma <shivansh29.sv@gmail.com>
2023-04-23 05:02:43 +0530

    test for prototype

--- plugin/plugin_test.go
+++ plugin/plugin_test.go
@@ -1,8 +1,11 @@
 //go:generate tinygo build -o ./testdata/hello-world.wasm -target=wasi ./testdata/hello-world/main.go
-package plugin
+package plugin_test
 
 import (
+	"fmt"
+	"net/http"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 
@@ -21,5 +24,37 @@ func TestRatelimit(t *testing.T) {
 	tx.ProcessResponseBody()
 	tx.ProcessResponseHeaders(200, "HTTP/1.1")
 	tx.ProcessLogging()
+
+	var tests = []struct {
+		url                string
+		expectedStatusCode int
+	}{
+		{"http://localhost:8090?id=2", 200},
+		{"http://localhost:8090?id=2", 200},
+		{"http://localhost:8090?id=2", 200},
+		{"http://localhost:8090?id=2", 200},
+		{"http://localhost:8090?id=2", 429},
+		{"http://localhost:8090?id=2", 429},
+		{"http://localhost:8090?id=2", 429},
+		{"http://localhost:8090?id=2", 429},
+		{"http://localhost:8090?id=2", 200},
+		{"http://localhost:8090?id=2", 200},
+	}
+
+	for i, test := range tests {
+		t.Run(fmt.Sprintf("Test %v", i), func(t *testing.T) {
+			res, err := http.Get(test.url)
+			if err != nil {
+				fmt.Printf("Error: %s", err)
+				t.Errorf("Error in %v, expected: %v, got: %v", test.url, test.expectedStatusCode, err.Error())
+			}
+
+			if res.StatusCode != test.expectedStatusCode {
+				t.Errorf("Error in %v, expected: %v, got: %v", test.url, test.expectedStatusCode, res.StatusCode)
+			}
+		})
+		time.Sleep(125 * time.Millisecond)
+	}
+
 	tx.Close()
 }
