8b1b14410372a0717bd63a929fc175d583aad247
shivansh verma <shivansh29.sv@gmail.com>
2023-06-20 14:47:28 +0530

    removed redundant code

--- magefile.go
+++ magefile.go
@@ -10,36 +10,6 @@ import (
 	_ "github.com/vermaShivansh/coraza-ratelimit-plugin/plugin"
 )
 
-// errUpdateGeneratedFiles:=errors.New("Error in generating files")
-// Lint verifies code quality.
-// func Lint() error {
-// 	if err := sh.RunV("go", "generate", "./..."); err != nil {
-// 		return err
-// 	}
-
-// 	if sh.Run("git", "diff", "--exit-code", "--", "'*.gen.go'") != nil {
-// 		return errUpdateGeneratedFiles
-// 	}
-
-// 	if err := sh.RunV("go", "run", fmt.Sprintf("github.com/golangci/golangci-lint/cmd/golangci-lint@%s", golangCILintVer), "run"); err != nil {
-// 		return err
-// 	}
-
-// 	if err := sh.RunV("go", "mod", "tidy"); err != nil {
-// 		return err
-// 	}
-
-// 	if err := sh.RunV("go", "work", "sync"); err != nil {
-// 		return err
-// 	}
-
-// 	if sh.Run("git", "diff", "--exit-code", "go.mod", "go.sum", "go.work", "go.work.sum") != nil {
-// 		return errRunGoModTidy
-// 	}
-
-// 	return nil
-// }
-
 // Test runs all tests.
 func Test() error {
 	// remove go test cache
