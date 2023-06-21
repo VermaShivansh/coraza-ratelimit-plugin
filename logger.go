30f7ebf3e4d524e16fc6444f69b8ecfaa3a7585f
shivansh verma <shivansh29.sv@gmail.com>
2023-06-21 14:45:13 +0530

    should give errors in actions

--- logger.go
+++ logger.go
@@ -1,27 +1,27 @@
 package main
 
-// import "github.com/corazawaf/coraza/v3/debuglog"
+import "github.com/corazawaf/coraza/v3/debuglog"
 
-// type errWriter struct {
-// 	debuglog.Logger
-// }
+type errWriter struct {
+	debuglog.Logger
+}
 
-// func (w errWriter) Write(p []byte) (n int, err error) {
-// 	if len(p) == 0 {
-// 		return 0, nil
-// 	}
-// 	w.Logger.Error().Msg(string(p))
-// 	return len(p), nil
-// }
+func (w errWriter) Write(p []byte) (n int, err error) {
+	if len(p) == 0 {
+		return 0, nil
+	}
+	w.Logger.Error().Msg(string(p))
+	return len(p), nil
+}
 
-// type outWriter struct {
-// 	debuglog.Logger
-// }
+type outWriter struct {
+	debuglog.Logger
+}
 
-// func (w outWriter) Write(p []byte) (n int, err error) {
-// 	if len(p) == 0 {
-// 		return 0, nil
-// 	}
-// 	w.Logger.Debug().Msg(string(p))
-// 	return len(p), nil
-// }
+func (w outWriter) Write(p []byte) (n int, err error) {
+	if len(p) == 0 {
+		return 0, nil
+	}
+	w.Logger.Debug().Msg(string(p))
+	return len(p), nil
+}
