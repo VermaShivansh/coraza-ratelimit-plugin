5943a1b9ccec673cc4850d6455ddaa363924502c
shivansh verma <shivansh29.sv@gmail.com>
2023-08-08 18:44:24 +0530

    comments added, using sonic for serializiation, code shifting

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/bsm/redislock v0.9.4
+	github.com/bytedance/sonic v1.9.2
 	github.com/corazawaf/coraza/v3 v3.0.0-rc.1.0.20230324141503-5c4beaf58d91
 	github.com/magefile/mage v1.15.0
 	github.com/redis/go-redis/v9 v9.0.5
@@ -12,11 +13,15 @@ require (
 
 require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/corazawaf/libinjection-go v0.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
 	github.com/petar-dambovaliev/aho-corasick v0.0.0-20211021192214-5ab2d9280aa9 // indirect
 	github.com/tidwall/gjson v1.14.4 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.1 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
 	rsc.io/binaryregexp v0.2.0 // indirect
 )
--- plugin/distribute.go
+++ plugin/distribute.go
@@ -2,11 +2,11 @@ package plugin
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"log"
 	"time"
 
+	"github.com/bytedance/sonic"
 	"github.com/redis/go-redis/v9"
 )
 
@@ -16,109 +16,122 @@ type Distrubute struct {
 	lastSync     int64
 }
 
+// it uses syncFunc declared below
+/*
+syncFunc does the core sync stuff
+it is ran initially to get the initial data from redis
+and runs syncFunc after every syncInterval
+*/
 func (e *Ratelimit) syncService() {
-	ticker := time.NewTicker(e.Distributed.SyncInterval)
-
-	syncFunc := func() error {
-		//read from the redis
-		fmt.Println("Syncing with Redis")
-
-		// currentTimeMilli := time.Now().UnixMilli()
-		//Obtain Redis lock
-		ctx := context.Background()
-
-		redisLock, err := ObtainLock(ctx, e.UniqueKey)
-		if err != nil {
-			return err
-		}
-		// fmt.Println("LOCK OBTAIN", time.Now().UnixMilli()-currentTimeMilli)
-
-		result, err := GetData(ctx, e.UniqueKey)
-		if err != nil && err != redis.Nil {
-			return fmt.Errorf("error while fetching from redis: %v", err.Error())
-		}
-		// fmt.Println("GET", time.Now().UnixMilli()-currentTimeMilli)
-
-		currentTime := time.Now().Unix()
-		syncedZones := Zones{}
-
-		// if key does not exist-> there is nothing to marshal
-		if err != redis.Nil {
-			err = json.Unmarshal([]byte(result), &syncedZones)
-			if err != nil {
-				return fmt.Errorf("error while unmarshling result from redis: %v", err.Error())
-			}
-		}
-
-		//update locally
-		e.mutex.Lock()
-
-		for zone_name, zone_events := range syncedZones {
-			for timestamp, events_in_timestamp := range zone_events {
-				// we dont care about the timestamps whose window is already passed
-				// we dont care about the timestamps which already have been synced
-
-				if timestamp > e.Distributed.lastSync && timestamp > (currentTime-e.Window) {
-					_, ok := e.Zones[zone_name]
-					if !ok {
-						// create macro for it and update zone_events in currenttimestamp
-						e.Zones[zone_name] = ZoneEvents{
-							timestamp: events_in_timestamp,
-						}
-						break
-					} else {
-						_, ok := e.Zones[zone_name][timestamp]
-						if !ok {
-							e.Zones[zone_name][timestamp] = events_in_timestamp
-						} else {
-							e.Zones[zone_name][timestamp] += events_in_timestamp
-						}
-					}
-				}
-			}
-		}
-		// e.Zones is completely synced now
-		syncedZones = e.Zones
-		e.mutex.Unlock()
-
-		log.Println("syncedZones", syncedZones)
-
-		jsonStrByteArray, err := json.Marshal(syncedZones)
-		if err != nil {
-			return fmt.Errorf("error encoding JSON: %v", err.Error())
-		}
-
-		// set in redis
-		err = SetData(ctx, e.UniqueKey, string(jsonStrByteArray))
-		if err != nil {
-			return fmt.Errorf("error in setting value in DB: %v", err.Error())
-		}
-		// fmt.Println("SET", time.Now().UnixMilli()-currentTimeMilli)
-
-		// Release the lock so other processes or threads can obtain a lock.
-		err = redisLock.Release(ctx)
-		if err != nil {
-			return err
-		}
-
-		// fmt.Println("LOCK RELEASE", time.Now().UnixMilli()-currentTimeMilli)
-
-		//updating last sync time with the time when we fetched data from redis
-		e.Distributed.lastSync = currentTime
-
-		return nil
-	}
 
 	//populate initial values of ZoneEvents
-	if err := syncFunc(); err != nil {
+	if err := syncFunc(e); err != nil {
 		fmt.Println(err)
 	}
 
+	ticker := time.NewTicker(e.Distributed.SyncInterval)
 	for {
 		<-ticker.C
-		if err := syncFunc(); err != nil {
+		if err := syncFunc(e); err != nil {
 			fmt.Println(err)
 		}
 	}
 
+}
+
+/*
+1. Obtain lock on redis for unique key (same throughout all the instances of application)
+2. Fetch ratelimit data
+3. update the ratelimit data with events of all timestamps>lastSync and timestamp > (currentTime-e.Window) (suppose syncing is done every minute but window is of 5seconds then we should not compute events of timestamp from 0th second - 55th second as they are redundant.)
+4. Set the updated states in Redis
+5. Release the lock
+*/
+func syncFunc(e *Ratelimit) error {
+	//read from the redis
+	fmt.Println("Syncing with Redis")
+
+	// currentTimeMilli := time.Now().UnixMilli()
+	//Obtain Redis lock
+	ctx := context.Background()
+
+	redisLock, err := ObtainLock(ctx, e.UniqueKey)
+	if err != nil {
+		return err
+	}
+	// fmt.Println("LOCK OBTAIN", time.Now().UnixMilli()-currentTimeMilli)
+
+	result, err := GetData(ctx, e.UniqueKey)
+	if err != nil && err != redis.Nil {
+		return fmt.Errorf("error while fetching from redis: %v", err.Error())
+	}
+	// fmt.Println("GET", time.Now().UnixMilli()-currentTimeMilli)
+
+	currentTime := time.Now().Unix()
+	syncedZones := Zones{}
+
+	// if key does not exist-> there is nothing to marshal
+	if err != redis.Nil {
+		err = sonic.Unmarshal([]byte(result), &syncedZones)
+		if err != nil {
+			return fmt.Errorf("error while unmarshling result from redis: %v", err.Error())
+		}
+	}
+
+	//update locally
+	e.mutex.Lock()
+
+	for zone_name, zone_events := range syncedZones {
+		for timestamp, events_in_timestamp := range zone_events {
+			// we dont care about the timestamps which are not in current window
+			// we dont care about the timestamps which already have been synced
+
+			if timestamp > e.Distributed.lastSync && timestamp > (currentTime-e.Window) {
+				_, ok := e.Zones[zone_name]
+				if !ok {
+					// create zone in local if doesn't exist and update zone_events in currenttimestamp
+					e.Zones[zone_name] = ZoneEvents{
+						timestamp: events_in_timestamp,
+					}
+					break
+				} else {
+					_, ok := e.Zones[zone_name][timestamp]
+					if !ok {
+						e.Zones[zone_name][timestamp] = events_in_timestamp
+					} else {
+						e.Zones[zone_name][timestamp] += events_in_timestamp
+					}
+				}
+			}
+		}
+	}
+	// e.Zones is completely synced now
+	syncedZones = e.Zones
+	e.mutex.Unlock()
+
+	log.Println("syncedZones", syncedZones)
+
+	jsonStrByteArray, err := sonic.Marshal(syncedZones)
+	if err != nil {
+		return fmt.Errorf("error encoding JSON: %v", err.Error())
+	}
+
+	// set in redis
+	err = SetData(ctx, e.UniqueKey, string(jsonStrByteArray))
+	if err != nil {
+		return fmt.Errorf("error in setting value in DB: %v", err.Error())
+	}
+	// fmt.Println("SET", time.Now().UnixMilli()-currentTimeMilli)
+
+	// Release the lock so other processes or threads can obtain a lock.
+	err = redisLock.Release(ctx)
+	if err != nil {
+		return err
+	}
+
+	// fmt.Println("LOCK RELEASE", time.Now().UnixMilli()-currentTimeMilli)
+
+	//updating last sync time with the time when we fetched data from redis
+	e.Distributed.lastSync = currentTime
+
+	return nil
 }
